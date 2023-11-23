74369d00927e5aa810764f68ec5a7508ae8c8c49
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-11-23 22:50:01 +0200

    Reading first migration - initial schema

--- CarAppBE/CarApp.DataAccess/Migrations/20231123204600_InitialSchema.Designer.cs
+++ CarAppBE/CarApp.DataAccess/Migrations/20231123204600_InitialSchema.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarApp.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20231123194506_InitialSchema")]
+    [Migration("20231123204600_InitialSchema")]
     partial class InitialSchema
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
