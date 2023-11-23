1f84183de64724ac023290f7340470c2b605f414
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-11-23 22:43:41 +0200

    creating handler (it binds a controller with a repository)

---
+++ CarAppBE/CarApp.Business/CarHandler.cs
@@ -0,0 +1,40 @@
+﻿using CarApp.Business.Entities;
+using CarApp.Business.Interfaces;
+
+namespace CarApp.Business
+{
+    public class CarHandler 
+    {
+        private IRepository<Car> repository;
+
+        public CarHandler(IRepository<Car> _repository)
+        {
+            repository = _repository;
+        }
+
+        public Task<IEnumerable<Car>> GetAll()
+        {
+            return repository.GetAll();
+        }
+
+        public async Task<Car> GetById(long id)
+        {
+            return await repository.GetById(id);
+        }
+
+        public async Task<Car> Add(Car entity)
+        {
+            return await repository.Add(entity);
+        }
+
+        public async Task Delete(Car entity)
+        {
+            await repository.Delete(entity);
+        }
+
+        public async Task<Car> Update(Car entity)
+        {
+            return await repository.Update(entity);
+        }
+    }
+}
