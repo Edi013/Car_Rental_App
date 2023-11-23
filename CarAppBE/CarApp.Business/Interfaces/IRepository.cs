8494e59f9d04e29a6ebb94fd8873ece931ba7aca
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-11-23 22:42:41 +0200

    Adding repository for Car table and abstraction for this interaction

---
+++ CarAppBE/CarApp.Business/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace CarApp.Business.Interfaces
+{
+    public interface  IRepository<T>
+    {
+        public Task<IEnumerable<T>> GetAll();
+        public Task<T> GetById(long id);
+        public Task<T> Add(T entity);
+        public Task<T> Update(T entity);
+        public Task Delete(T entity);
+    }
+}
