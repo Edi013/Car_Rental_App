83835859520f727f9caef2a58656b2ecac1449db
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-11-24 09:52:12 +0200

    Implementing SHA256 hash function, it needed to refactor the user table to a string password

--- CarAppBE/CarApp.Business/UserHandler.cs
+++ CarAppBE/CarApp.Business/UserHandler.cs
@@ -1,9 +1,6 @@
 ﻿using CarApp.Business.DTO;
 using CarApp.Business.Entities;
 using CarApp.DataAccess.Repositories;
-using System.Collections;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace CarApp.Business
 {
@@ -44,24 +41,8 @@ namespace CarApp.Business
             return new User()
             {
                 UserName = dto.UserName,
-                Password = GetHash(dto.Password)
+                Password = SHA256Hash.GetHash(dto.Password)
             };
-        }
-        private string GetHash(string input)
-        {
-            using (SHA256 sha256Hash = SHA256.Create())
-            {
-                byte[] data = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
-
-                StringBuilder builder = new StringBuilder();
-
-                for (int i = 0; i < data.Length; i++)
-                {
-                    builder.Append(data[i].ToString("x2"));
-                }
-
-                return builder.ToString();
-            }
         }
     }
 }
