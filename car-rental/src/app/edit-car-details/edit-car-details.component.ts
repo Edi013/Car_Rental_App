5de699a594fa51b1ef23b5f81b67bc307804e33f
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-11-24 23:00:17 +0200

    adding new component for editing a car details

---
+++ car-rental/src/app/edit-car-details/edit-car-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-car-details',
+  templateUrl: './edit-car-details.component.html',
+  styleUrls: ['./edit-car-details.component.scss']
+})
+export class EditCarDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
